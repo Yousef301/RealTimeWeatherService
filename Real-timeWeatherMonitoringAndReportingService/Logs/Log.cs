e44f001b46ace156c2f282219fa181c1e5f0a7d6
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-03-26 20:41:44 +0200

    Add log class which will help in console logging.

---
+++ Real-timeWeatherMonitoringAndReportingService/Logs/Log.cs
@@ -0,0 +1,10 @@
+﻿namespace Real_timeWeatherMonitoringAndReportingService.Logs;
+
+public class Log
+{
+    public static void BotActivatedMessage(string botName, string message)
+    {
+        Console.WriteLine($"{botName} activated!");
+        Console.WriteLine($"{botName}: {message}");
+    }
+}
