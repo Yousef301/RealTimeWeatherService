211f1154fff496a2ce1f3c163d13a8c5c6a02f4f
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-03-26 20:43:36 +0200

    Create Test class.
    Test class has static method on it called Run, it used to test if the program is running correctly.

---
+++ Real-timeWeatherMonitoringAndReportingService/Presntation/Test.cs
@@ -0,0 +1,54 @@
+﻿using Real_timeWeatherMonitoringAndReportingService.Configuration;
+using Real_timeWeatherMonitoringAndReportingService.Models;
+using Real_timeWeatherMonitoringAndReportingService.Services;
+
+namespace Real_timeWeatherMonitoringAndReportingService.Presntation;
+
+public class Test
+{
+    private static Dictionary<string, IWeatherBot> _bots = BotsConfiguration.LoadConfigurations();
+
+    public static void Run()
+    {
+        string userInput;
+
+        WeatherStation weatherStation = new WeatherStation();
+
+        WeatherStationService.InitializeObservers(weatherStation, _bots);
+        do
+        {
+            Console.WriteLine("Enter weather data:");
+            userInput = Console.ReadLine();
+            Console.Clear();
+
+            var type = Helpers.IdentifyFormat.GetDataFormat(userInput);
+
+            var deserializerFactory = new WeatherDeserializerFactory();
+            var weatherDeserializer = deserializerFactory.CreateDeserializer(type);
+
+
+            if (weatherDeserializer is not null)
+            {
+                WeatherStation tempWeatherStation = weatherDeserializer.DeserializeWeatherInfo(userInput);
+
+                UpdateWeatherStation(weatherStation, tempWeatherStation);
+
+                foreach (var bot in _bots.Values)
+                {
+                    bot.WeatherStation = weatherStation;
+                }
+
+                weatherStation.Notify();
+            }
+
+            Console.WriteLine();
+        } while (!userInput.Equals("q"));
+    }
+
+    private static void UpdateWeatherStation(WeatherStation weatherStation, WeatherStation newWeatherData)
+    {
+        weatherStation.Temperature = newWeatherData.Temperature;
+        weatherStation.Humidity = newWeatherData.Humidity;
+        weatherStation.Location = newWeatherData.Location;
+    }
+}
