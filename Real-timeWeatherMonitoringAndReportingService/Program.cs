b4635e1e541e55586319a552379e830afb27a5c4
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-03-26 20:43:57 +0200

    Update Program.cs

--- Real-timeWeatherMonitoringAndReportingService/Program.cs
+++ Real-timeWeatherMonitoringAndReportingService/Program.cs
@@ -1,8 +1,13 @@
-﻿namespace Real_timeWeatherMonitoringAndReportingService;
+﻿using Real_timeWeatherMonitoringAndReportingService.Configuration;
+using Real_timeWeatherMonitoringAndReportingService.Models;
+using Real_timeWeatherMonitoringAndReportingService.Presntation;
+
+namespace Real_timeWeatherMonitoringAndReportingService;
 
 public class Program
 {
     static void Main(string[] args)
     {
+        Test.Run();
     }
 }
