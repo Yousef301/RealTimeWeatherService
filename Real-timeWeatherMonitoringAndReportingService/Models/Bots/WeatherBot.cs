d742eabfd14ec4a55a47acd893901da026dbffd5
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-03-31 15:49:51 +0200

    Add WeatherBot class as an abstract class for other bots.

---
+++ Real-timeWeatherMonitoringAndReportingService/Models/Bots/WeatherBot.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Real_timeWeatherMonitoringAndReportingService.Interfaces;
+using Real_timeWeatherMonitoringAndReportingService.Models.Weather;
+using Real_timeWeatherMonitoringAndReportingService.Services;
+
+namespace Real_timeWeatherMonitoringAndReportingService.Models.Bots;
+
+public abstract class WeatherBot : IWeatherBot
+{
+    [Required(ErrorMessage = "Activated is required.")]
+    public bool Activated { get; set; }
+
+    [Required(ErrorMessage = "Message is required.")]
+    [StringLength(100, MinimumLength = 10, ErrorMessage = "Message length must be between 10 and 100 characters.")]
+    public string Message { get; set; }
+
+    public WeatherStation WeatherStation { get; set; }
+
+    [Required(ErrorMessage = "BotService is required.")]
+    public BotServices BotService { get; init; }
+
+    public void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
