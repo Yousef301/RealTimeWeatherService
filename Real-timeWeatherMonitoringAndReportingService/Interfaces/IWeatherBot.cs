8aafb57856c50592fdbec7c405547c5e47e33a9f
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-03-31 15:56:21 +0200

    Add Activated and BotService property to the IWeatherBot interface.
    Now you can check the status of the bot's activity by accessing its 'Activated' property. Additionally, rather than creating an instance of the BotService within the class itself, which violates DIP, the service is now injected into the bot through creation.

--- Real-timeWeatherMonitoringAndReportingService/Interfaces/IWeatherBot.cs
+++ Real-timeWeatherMonitoringAndReportingService/Interfaces/IWeatherBot.cs
@@ -1,11 +1,14 @@
 ﻿using Real_timeWeatherMonitoringAndReportingService.Models.Weather;
+using Real_timeWeatherMonitoringAndReportingService.Services;
 
 namespace Real_timeWeatherMonitoringAndReportingService.Interfaces;
 
 public interface IWeatherBot
 {
+    public bool Activated { get; set; }
     public string Message { get; set; }
     public WeatherStation WeatherStation { get; set; }
+    public BotServices BotService { get; init; }
 
     public void Update();
 }
