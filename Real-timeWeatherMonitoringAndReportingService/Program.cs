22b834895955f2729f5a1b440de7ca63fd8b6d3a
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-03-31 10:47:39 +0200

    Update Program.cs so it's now responsible for running the program.

--- Real-timeWeatherMonitoringAndReportingService/Program.cs
+++ Real-timeWeatherMonitoringAndReportingService/Program.cs
@@ -1,13 +1,49 @@
 ﻿using Real_timeWeatherMonitoringAndReportingService.Configuration;
+using Real_timeWeatherMonitoringAndReportingService.Deserializers;
 using Real_timeWeatherMonitoringAndReportingService.Models;
-using Real_timeWeatherMonitoringAndReportingService.Presntation;
+using Real_timeWeatherMonitoringAndReportingService.Outputter;
+using Real_timeWeatherMonitoringAndReportingService.Reader;
 
 namespace Real_timeWeatherMonitoringAndReportingService;
 
 public class Program
 {
+    private static readonly Dictionary<string, IWeatherBot> _bots = BotsConfiguration.LoadConfigurations();
+
     static void Main(string[] args)
     {
-        Test.Run();
+        string? userInput;
+
+        var weatherStation = new WeatherStation(_bots.Values.ToList());
+
+        do
+        {
+            ConsoleOutput.MessageOutput("Enter weather data:");
+            userInput = ConsoleInputReader.ReadInput();
+            Console.Clear();
+
+            var weatherData = TryDeserializeInput(userInput);
+
+            if (weatherData is not null)
+            {
+                weatherStation.Notify(weatherData);
+            }
+            else
+                ConsoleOutput.MessageOutput("Invalid data format.");
+
+            ConsoleOutput.MessageOutput("");
+        } while (!userInput.Equals("q"));
+    }
+
+    private static WeatherData? TryDeserializeInput(string userInput)
+    {
+        var deserializers = new List<IWeatherDeserializer>()
+        {
+            new JsonWeatherDeserializer(),
+            new XmlWeatherDeserializer()
+        };
+
+        return deserializers.Select(deserializer => deserializer.TryDeserializeWeatherInfo(userInput))
+            .OfType<WeatherData>().FirstOrDefault();
     }
 }
